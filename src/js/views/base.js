abd36fa53281a0caf5a026ba4240f97f6e9cceba
Mark Allen <markdallen@me.com>
2020-09-06 14:35:13 +0100

    add list of elements in base js file

---
+++ src/js/views/base.js
@@ -0,0 +1,9 @@
+// Elements that already exist in the DOM
+export const elements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  results: document.querySelector('.results'),
+  resultsCount: document.querySelector('.results__count'),
+  resultsList: document.querySelector('.results__list'),
+  searchFooter: document.querySelector('.search-footer'),
+};
